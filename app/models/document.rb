4724e89a47c2771646c05819f26cb48b2d228f08
ma2ma <steve2008.ma@gmail.com>
2014-03-13 23:16:54 +0800

    解决目录属性页面排列问题，想要建立笔记类型目录页面

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,14 +3,17 @@ class Document
   include Mongoid::Timestamps
   include Mongoid::BaseModel
 
-  field :name
-  field :description
+  field :title
+  field :summary
+  field :content_have_attr
+  field :content_html
+  field :dirty_flag, :type => Boolean , :default => false #判断是否有属性被修改，如果被修改，则看内容时需要重新编译
   field :sort, :type => Integer, :default => 0
 
   belongs_to :folder
   has_and_belongs_to_many :third_disciplines
   has_and_belongs_to_many :tags
 
-  validates_presence_of :name
+  validates_presence_of :title
 
 end
