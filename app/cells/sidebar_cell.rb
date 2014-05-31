4999ebf839008675be88b71b702761e4a4900a0d
ma2ma <steve2008.ma@gmail.com>
2014-05-31 16:25:59 +0800

    前端开放目录功能完成

--- app/cells/sidebar_cell.rb
+++ app/cells/sidebar_cell.rb
@@ -9,6 +9,8 @@ class SidebarCell < Cell::Rails
   def folders(args)
   	@home_user=args[:huser]
     @current_user=args[:cuser]
+    @cur_folder= args[:cur_folder]
+    @public_folders=@home_user.folders.all.entries.find_all{|n| n.visiable?(@current_user) } if @home_user
     render
   end
   def topdocs(args)
--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -7,19 +7,19 @@ class FoldersController < ApplicationController
   end
   def show
   	begin
-  		folder = Folder.find(params[:id])
+  		@folder = Folder.find(params[:id])
   		documents =[]
       @query_key=params[:q]
       if @query_key.blank?
-        @all_docs=folder.documents.all
+        @all_docs=@folder.documents.all
       else
-        @all_docs=folder.documents.all.where(title: /.*#{@query_key}.*/)
+        @all_docs=@folder.documents.all.where(title: /.*#{@query_key}.*/)
       end
   		@all_docs.each do |doc| 
         documents.push doc if doc.visiable?(current_user)
       end
   		@docs = Kaminari.paginate_array(documents).page(params[:page]).per(10)
-      @home_user = folder.user
+      @home_user = @folder.user
   	rescue
   		render_404
   	end
