ca73a50a7cde281630c1e07104d4aa1b51d7fd73
ma2ma <steve2008.ma@gmail.com>
2014-06-20 09:45:57 +0800

    修改todolist颜色

--- app/models/todolist.rb
+++ app/models/todolist.rb
@@ -34,11 +34,11 @@ class Todolist
   def get_color
     case (self.start-Date.today).to_f
     when 0...1
-      'danger'
-    when 1...2
-      'warning'
-    when 2...6
       'success'
+    when 1...2
+      'danger'
+    when 2...6
+      'warning'
     when 6...30
       'primary'
     when 30...9999999
@@ -50,11 +50,11 @@ class Todolist
   def get_color_value
     case (self.start-Date.today).to_f
     when 0...1
-      '#f56954'
-    when 1...2
-      '#f39c12'
-    when 2...6
       '#00a65a'
+    when 1...2
+      '#f56954'
+    when 2...6
+      '#f39c12'
     when 6...30
       '#0073b7'
     when 30...9999999
